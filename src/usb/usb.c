53ae6e468aa468918e595331e738666a815cfe24
danieru <danieru.dressler@gmail.com>
2014-05-30 15:29:58 -0600

    Verify usb device has ipp usb interfaces

--- src/usb/usb.c
+++ src/usb/usb.c
@@ -26,20 +26,69 @@ usb_sock_t *open_usb()
 	}
 
 
+	int selected_config = -1;
+	int selected_ipp_interface_count = 0;
 	libusb_device *printer_device = NULL;
 	for (ssize_t i = 0; i < device_count; i++) {
 		libusb_device *candidate = device_list[i];
 		struct libusb_device_descriptor desc;
 		libusb_get_device_descriptor(candidate, &desc);
 		// TODO: error if device is not printer supporting IPP
-		if (desc.idVendor == 0x03f0 &&
-			desc.idProduct == 0xc511
-			// TODO: filter on serial
-			) {
-			printer_device = candidate;
-			break;
+		if (desc.idVendor != 0x03f0 &&
+		    desc.idProduct != 0xc511)
+			continue;
+		// TODO: filter on serial number
+
+		for (uint8_t config_num = 0;
+		     config_num < desc.bNumConfigurations;
+		     config_num++) {
+			struct libusb_config_descriptor *config = NULL;
+			status = libusb_get_config_descriptor(candidate,
+			                                      config_num,
+			                                      &config);
+			int ippusb_interface_count = 0;
+			for (uint8_t interface_num = 0;
+			     interface_num < config->bNumInterfaces;
+			     interface_num++) {
+
+				const struct libusb_interface *interface = NULL;
+				interface = &config->interface[interface_num];
+
+				for (int alt_num = 0;
+				     alt_num < interface->num_altsetting;
+				     alt_num++) {
+
+					const struct libusb_interface_descriptor *alt = NULL;
+					alt = &interface->altsetting[alt_num];
+
+					// Check for IPP over USB interfaces
+					if (alt->bInterfaceClass != 0x07 ||
+					    alt->bInterfaceSubClass != 0x01 ||
+					    alt->bInterfaceProtocol != 0x04)
+						continue;
+
+					ippusb_interface_count++;
+					break;
+				}
+			}
+			if (ippusb_interface_count >= 2) {
+				selected_config = config_num;
+				selected_ipp_interface_count =
+				                           ippusb_interface_count;
+				printer_device = candidate;
+				goto found_target_device;
+			}
+			if (ippusb_interface_count == 1) {
+				ERR("usb device has only one ipp interface "
+				    "in violation of standard");
+				goto error;
+			}
+			ERR("usb device had no ipp-usb class interfaces");
+			// TODO: if VID and PID set warn 
+			// that the device is not a ipp printer
 		}
 	}
+found_target_device:
 
 	if (printer_device == NULL) {
 		ERR("no printer found by that vid & pid & serial");
--- src/usb/usb.h
+++ src/usb/usb.h
@@ -5,6 +5,9 @@
 typedef struct {
 	libusb_context *context;
 	libusb_device_handle *printer;
+
+	uint32_t num_interfaces;
+	struct libusb_interface *interfaces;
 } usb_sock_t;
 
 usb_sock_t *open_usb();
