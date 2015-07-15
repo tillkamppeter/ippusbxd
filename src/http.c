3facde24a3b74168047dcd564bf609fd9911edcb
Till Kamppeter <till.kamppeter@gmail.com>
2015-07-15 20:40:55 -0300

    Fixed bug in chunked transfers to the printer
    Chunked HTTP transfers allow streaming and therefore they are used to
    send larger amounts of data (Usually print jobs) to the printer. There
    was a bug that made an HTTP message being considered completed when
    the buffer space was full, ignoring the fact that packages can be
    larger and that therefore the space gets expanded if necessary. This
    made print jobs (except tiny ones, probably the ones smaller than the
    initial buffer size of 4kB) hanging but instructions and queries
    (which are transfered in content-length mode) working normally.

--- src/http.c
+++ src/http.c
@@ -64,11 +64,8 @@ static void packet_check_completion(struct http_packet_t *pkt)
 	if (pkt->expected_size && pkt->filled_size >= pkt->expected_size)
 		pkt->is_completed = 1;
 
-	// Pkt at capacity
-	if (pkt->filled_size == pkt->buffer_capacity) {
-		pkt->is_completed = 1;
-		msg->is_completed = 1;
-	} else if (pkt->filled_size > pkt->buffer_capacity) {
+	// Pkt over capacity
+	if (pkt->filled_size > pkt->buffer_capacity) {
 		// Santiy check
 		ERR_AND_EXIT("Overflowed packet buffer");
 	}
@@ -501,8 +498,6 @@ pending_known:
 		ERR_AND_EXIT("Expected cannot be larger than filled");
 
 	size_t pending = expected - pkt->filled_size;
-	
-	packet_check_completion(pkt);
 
 	// Expand buffer as needed
 	while (pending + pkt->filled_size > pkt->buffer_capacity) {
@@ -516,6 +511,8 @@ pending_known:
 			return 0;
 		}
 	}
+
+	packet_check_completion(pkt);
 
 	return pending;
 }
